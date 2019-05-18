ece80565f438929989d53be76a7c0df9bb4c35ec
NoeCR <noecr.dev@gmail.com>
2019-05-18 21:42:53 +0200

    Upload Firechat Fix merge

---
+++ 09-firechat/src/environments/environment.ts
@@ -0,0 +1,17 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAJlDYlSyqlpm-Q-MuWdwArlBJ3w7N3dxM",
+    authDomain: "firechat-2f7ce.firebaseapp.com",
+    databaseURL: "https://firechat-2f7ce.firebaseio.com",
+    projectId: "firechat-2f7ce",
+    storageBucket: "firechat-2f7ce.appspot.com",
+    messagingSenderId: "207817944831",
+    appId: "1:207817944831:web:d192d0e0783bc436"
+  }
+};
